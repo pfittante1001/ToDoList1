dcd93409be022decf7f1761f3d8d22ef186f4195
pfittante <pfittante@yahoo.com>
2017-07-26 14:41:02 -0400

    complete model controller and views

---
+++ ToDoList1/Models/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList1.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ ToDoList1/Models/ToDoList1Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList1.Models
+{
+    public class ToDoList1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ToDoList1Context() : base("name=ToDoList1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<ToDoList1.Models.List> Lists { get; set; }
+
+        public System.Data.Entity.DbSet<ToDoList1.Models.Task> Tasks { get; set; }
+    }
+}
